640cfe40e7be79a815d4f2807374d23018dd0d54
Daniel Carvajal <carvajal.dev@hotmail.com>
2025-09-23 19:15:18 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,4 +4,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta http://localhost:${PORT}`);
+  console.log(`📚 Documentação Swagger disponível em http://localhost:${PORT}/api-docs`);
 });
